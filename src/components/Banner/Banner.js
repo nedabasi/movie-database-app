7748050199043325d8146b6bd428d5e820b8e853
nedabasi <59399428+nedabasi@users.noreply.github.com>
2024-07-30 15:38:23 +0200

    feat: Add API request to fetch Netflix Originals using Axios
    - Set up Axios instance for TMDB API requests.
    - Define API requests in a separate module.
    - Fetch Netflix Originals in Banner component.
    - Display a random movie from the fetched data in the Banner component.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Banner.css";
-
-
+import axios from "../../axios";
+import requests from '../../Request';
 
 function Banner() {
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                const movies = request.data.results;
+                setMovie(
+                    movies[Math.floor(Math.random() * movies.length-1)]
+                );
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(movie);
+
     function truncate(str, n) {
-        if (str.length <= n)
-            return str;
-        return str.slice(0, n-1)+'...'
+        return str?.length > n ? str.slice(0, n - 1) + '...' : str;
     }
 
-  return (
-    <header className='banner' style={{
-        backgroundSize: "cover",
-        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg)`,
-        backgroundPosition: "center",
-    }} >
-        <div className='banner-contents'>
-            <h1 className='banner-title'>
-                Movie Name
-            </h1>
-            <button className='banner-button'>Play</button>
-            <button className='banner-button'>Msy List</button>
-        <h1 className='banner-decription'> {truncate(`This is a test description.`, 100)}</h1>
-        </div>
-        <div className='banner-fadeBottom'/>
-
-    </header>
-  )
+    return (
+        <header className='banner' style={{
+            backgroundSize: "cover",
+            backgroundImage: movie ? `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")` : `url("https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg")`,
+            backgroundPosition: "center",
+        }}>
+            <div className='banner-contents'>
+                <h1 className='banner-title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <button className='banner-button'>Play</button>
+                <button className='banner-button'>My List</button>
+                <h1 className='banner-decription'>{truncate(movie?.overview, 150)}</h1>
+            </div>
+            <div className='banner-fadeBottom' />
+        </header>
+    );
 }
 
 export default Banner;
