211d20fa6d6c7cacda73b97535e2e03fd594f8c7
nedabasi <59399428+nedabasi@users.noreply.github.com>
2024-08-01 17:46:09 +0200

    feat: Add Row component and integrate with HomeScreen
    - Added Row component to dynamically display movie rows with titles and images
    - Integrated Row component in HomeScreen to display various categories of movies
    - Implemented conditional rendering for movie posters and backdrops based on isLargeRow prop
    - Added CSS styling for Row component and handled responsive behavior and hover effects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import tmdb from './api/tmdb';
 import SearchBar from './components/SearchBar/SearchBar';
 import MovieList from './components/MovieList/MovieList';
 import HomeScreen from './components/HomeScreen/HomeScreen';
+import "./App.css";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -2,12 +2,41 @@ import React from 'react';
 import './HomeScreen.css'
 import Nav from '../Nav/Nav';
 import Banner from '../Banner/Banner';
+import requests from '../../Request';
+import Row from '../Row/Row';
 
 function HomeScreen() {
   return (
     <div className='HomeScreen'>
-         <Nav/>
-         <Banner/>
+        <Nav/>
+        <Banner/>
+        <Row
+         title = "HONEY ORIGINAL"
+         fetchUrl={requests.fetchNetflixOriginals}
+         isLargeRow
+         />
+
+        <Row
+         title = "Fetching Now" fetchUrl={requests.fetchTrending}/>
+          
+        <Row
+         title = "Top Rated"
+         fetchUrl={requests.fetchTopRated}/>
+        <Row
+         title = "Action Movies"
+         fetchUrl={requests.fetchActionMovies}/>
+        <Row
+         title = "Comedy Movies"
+         fetchUrl={requests.fetchComedyMovies}/>
+        <Row
+         title = "Horror Movies"
+         fetchUrl={requests.fetchHorrorMovies}/>
+        <Row
+         title = "Romance Movies"
+         fetchUrl={requests.fetchRomanceMovies}/>
+        <Row
+         title = "Documentaries"
+         fetchUrl={requests.fetchDocumentaries}/>
     </div>
   )
 }
